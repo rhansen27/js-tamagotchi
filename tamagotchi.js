28527d38155fcca15f2089abef314e4cc11738fc
rph <rhansen0127@gmail.com>
2024-04-30 23:35:16 -0500

    created the basic class

---
+++ tamagotchi.js
@@ -0,0 +1,31 @@
+class Tamagotchi {
+  constructor(name) {
+    this.name = name;
+    this.hunger = 0;
+    this.sleepiness = 0;
+    this.boredom = 0;
+    this.age = 0;
+    this.alive = true;
+  }
+  feed() {
+    this.hunger--;
+  }
+  sleep() {
+    this.sleepiness--;
+  }
+  play() {
+    this.boredom--;
+  }
+  ageUp() {
+    this.age++;
+  }
+  die() {
+    console.log("Your Tamagotchi has died!");
+    this.alive = false;
+  }
+}
+
+var newDate = new Date();
+let time = newDate.getTime();
+
+console.log(time);
